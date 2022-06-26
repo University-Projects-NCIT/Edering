9265972d3dde71e155b55f1b03550e40d4e2fdaf
karkigrishmin <grishminkarki7@gmail.com>
2022-06-26 23:04:02 +0545

    add custom values

--- edering_frontend/tailwind.config.js
+++ edering_frontend/tailwind.config.js
@@ -1,17 +1,49 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable global-require */
-const defaultTheme = require("tailwindcss/defaultTheme");
+// const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	mode: "jit",
-	purge: {
-		mode: "all",
-		content: ["./src/**/*.{svelte,jsx,js,ts,html}"],
-	},
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
+  mode: 'jit',
+  purge: {
+    mode: 'all',
+    content: ['./src/**/*.{svelte,jsx,js,ts,html}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      padding: {
+        btnNormalX: '2rem',
+        btnNormalY: '0.75rem',
+        btnMediumX: '1.6rem',
+        btnSmallX: '1rem',
+        btnSmallY: '0.5rem',
+      },
+      borderWidth: {
+        1: '1rem',
+        3: '3px',
+      },
+      borderRadius: {
+        buttonRadius: '1.875rem',
+      },
+      colors: {
+        yellow: {
+          primary: '#FFA800',
+        },
+        black: {
+          primary: '#333333',
+          secondary: '#808080',
+          dark: '#000',
+        },
+        gray: {
+          borderGray: '#EAEAEA',
+          bgGray2: '#F8F8F8',
+          primary: '#F2F2F2',
+          secondary: '#DDDDDD',
+          tertiary: '#AAAAAA',
+        },
+      },
+    },
+  },
 
-	plugins: [],
+  plugins: [],
 };
