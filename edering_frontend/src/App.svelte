febeb9b77ad57ee8960201c810eda0b15d8d7e69
karkigrishmin <grishminkarki7@gmail.com>
2022-08-01 09:21:50 +0545

    change directory name

--- edering_frontend/src/App.svelte
+++ edering_frontend/src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Button from 'components/Buttons/Button.svelte';
+  import Button from './components/buttons/Button.svelte';
 </script>
 
 <Button variant="transparent" label="Order" />
