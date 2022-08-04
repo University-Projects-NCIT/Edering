24a3f373a9e7353c0e1c6988b8e8ef4e1ff2a17b
karkigrishmin <grishminkarki7@gmail.com>
2022-08-04 19:30:24 +0545

    add fixed bottom nav to all pages

--- edering_frontend/src/routes/_layout.svelte
+++ edering_frontend/src/routes/_layout.svelte
@@ -1,4 +1,11 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import Box from 'components/layouts/Box.svelte';
+
+  import BottomFixedNav from 'pages/home/components/BottomFixedNav.svelte';
+</script>
 
 <slot />
-<footer>footer</footer>
+<!-- <footer>footer</footer> -->
+<Box className="fixed bottom-0 w-full">
+  <BottomFixedNav />
+</Box>
