97928de033fdd8876993ab58ecb86c85973ba844
karkigrishmin <grishminkarki7@gmail.com>
2022-08-04 19:23:39 +0545

    create input component

---
+++ edering_frontend/src/components/input_components/Input.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Box from 'components/layouts/Box.svelte';
+  export let placeholder = '';
+</script>
+
+<Box
+  flow="horizontal"
+  align="center"
+  className="bg-gray-primary rounded-full text-xs border-2 focus-within:border-yellow-primary focus:outline-none focus:ring-transparent border-gray-primary"
+>
+  <input
+    class="bg-gray-primary text-black-primary px-3 py-1 w-full rounded-full border-0 focus:placeholder-transparent focus:outline-none focus:ring-transparent"
+    type="text"
+    id="search-bar"
+    name="searchText"
+    {placeholder}
+  />
+  <Box className="px-2">
+    <img src="/icons/search-icon.svg" alt="search icon" />
+  </Box>
+</Box>
