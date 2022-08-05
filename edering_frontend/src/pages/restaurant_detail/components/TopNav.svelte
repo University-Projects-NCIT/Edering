fd984b417fd212fedccd3e9066e1ce3304e91ac5
karkigrishmin <grishminkarki7@gmail.com>
2022-08-05 23:55:30 +0545

    create top navbar component

---
+++ edering_frontend/src/pages/restaurant_detail/components/TopNav.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import Box from 'components/layouts/Box.svelte';
+  import type { IFoodCategory } from 'types/foodCategory.types';
+
+  type NavType = 'top' | 'foodCategory';
+  export let navType: NavType = 'top';
+  export let navItems: string[] | undefined = undefined;
+  export let foodCategories: IFoodCategory[] | undefined = undefined;
+</script>
+
+<Box flow="horizontal" align="center" className="space-x-2">
+  {#if navType === 'top'}
+    {#each navItems ?? [] as item}
+      <Box
+        className="rounded-xl border border-gray-primary py-2 px-4 {item ===
+        navItems?.[0]
+          ? 'border-yellow-primary'
+          : ''}"
+      >
+        <p style="font-size: 0.6rem; line-height: 0.6rem">
+          {item}
+        </p>
+      </Box>
+    {/each}
+  {:else}
+    {#each foodCategories ?? [] as category}
+      <Box
+        className="rounded-xl bg-gray-primary py-2 px-4 {category?.id ===
+        foodCategories?.[0]?.id
+          ? 'bg-yellow-primary text-white'
+          : ''}"
+      >
+        <p style="font-size: 0.6rem; line-height: 0.6rem">
+          {category?.c_name}
+        </p>
+      </Box>
+    {/each}
+  {/if}
+</Box>
