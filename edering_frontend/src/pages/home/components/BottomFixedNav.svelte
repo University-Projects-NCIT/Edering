d9c02777bad0055472583f0a5f7718daffc0552c
Jeevan Rupacha <rupachasgvn@gmail.com>
2022-09-22 13:40:15 +0545

    feat: login screen add

--- edering_frontend/src/pages/home/components/BottomFixedNav.svelte
+++ edering_frontend/src/pages/home/components/BottomFixedNav.svelte
@@ -23,18 +23,20 @@
 
 <Box
   flow="horizontal"
-  className="bg-gray-primary py-1"
+  className="bg-gray-primary pt-2 pb-2"
   align="center"
   justify="around"
 >
   {#each menuItems as item}
-    <a href="/">
+    <div>
+      <a href="/">
       <Box flow="vertical" align="center">
         <img class="w-4" src={item.icon} alt="" />
         <p class="mt-1" style="font-size: 0.6rem; line-height: 0.7rem">
           {item.name}
         </p>
       </Box>
-    </a>
+      </a>
+    </div>
   {/each}
 </Box>
---
+++ edering_frontend/src/routes/test/index.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Button from "components/buttons/Button.svelte";
+
+  
+  let count = 0 
+  const click = () => {
+    count = count + 1
+  }
+
+</script>
+
+<Button label="click" size="small"></Button>
+
+<style>
+  h1{
+    font-size: 32px;
+    color: blueviolet;
+  }
+</style>
