63ebc307cd5a1103c1d717e4bd762aa3678befa0
karkigrishmin <grishminkarki7@gmail.com>
2022-09-25 00:53:19 +0545

    fixes

---
+++ edering_frontend/src/components/index.ts
@@ -0,0 +1,3 @@
+export * from './buttons';
+export * from './input_components';
+export * from './layouts';
---
+++ edering_frontend/src/pages/restaurant_detail/menu/components/index.ts
@@ -0,0 +1,1 @@
+export { default as IncDecItemQuantity } from './IncDecItemQuantity.svelte';
