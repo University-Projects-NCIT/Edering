749d102f510d5761f5d324341b075ccfb36ec460
karkigrishmin <grishminkarki7@gmail.com>
2022-09-25 23:36:41 +0545

    api call test

--- edering_frontend/src/routes/index.svelte
+++ edering_frontend/src/routes/index.svelte
@@ -1,7 +1,35 @@
-<script>
-  import { goto } from '@roxi/routify';
+<script lang="ts">
+  import { request } from 'helper/request';
 
   import HomePage from 'pages/home/Home.svelte';
+  // import { onMount } from 'svelte';
+  import { ApiRequestMethods, IUserData } from 'types';
+
+  // onMount(async () => {
+  //   let res = await request<IUserData>({
+  //     url: '/customers/',
+  //     method: ApiRequestMethods.post,
+  //     data: {
+  //       id: '123',
+  //       auth_id: '123456789',
+  //     },
+  //   });
+  //   console.log('res', res);
+  // });
+
+  const apiCall = async () => {
+    const response = await request<IUserData>({
+      url: '/customers/',
+      method: ApiRequestMethods.post,
+      data: {
+        id: '64',
+        auth_id: '89',
+      },
+    });
+
+    console.log('response', response);
+  };
 </script>
 
+<button on:click={() => apiCall()}>Post</button>
 <HomePage />
