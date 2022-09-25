16f2c936f742dc761746fef64c46b8c4ef90bdb3
karkigrishmin <grishminkarki7@gmail.com>
2022-09-25 23:27:32 +0545

    update config

--- edering_frontend/src/config/envData.ts
+++ edering_frontend/src/config/envData.ts
@@ -1,13 +1,14 @@
-export const envData = {
+const envData = {
   firebase: {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.FIREBASE_API_KEY?.trim(),
+    authDomain: 'edering-authentication.firebaseapp.com'.trim(),
+    projectId: process.env.FIREBASE_PROJECT_ID?.trim(),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET?.trim(),
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID?.trim(),
+    appId: process.env.FIREBASE_APP_ID?.trim(),
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID?.trim(),
   },
+  baseURL: process.env.BASE_URL,
 };
 
-export default {};
+export default envData;
