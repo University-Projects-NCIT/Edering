c32e570bcf8383503662e17c85597e33a6dbf430
Jeevan Rupacha <rupachasgvn@gmail.com>
2022-10-21 22:58:58 +0545

    feat: message input field to update msg

--- edering_frontend/src/routes/chat/index.svelte
+++ edering_frontend/src/routes/chat/index.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+const s = `<script lang="ts">
   import { db } from 'config/conn_firebase';
   import { user_store } from 'store';
   import type { IMessage, IUser } from 'types';
@@ -9,32 +9,20 @@
     console.log(user);
   });
 
-  let msg = '';
-  let result: IMessage[] = [];
+  let messageInput = ""
+  let msgData: IMessage[] = []
 
-  const hotelId = 'hotelid2';
+  const providerId = 'hotelid2';
   const customerId = 'customerid1';
 
   db.collection('db_messages')
-    .doc(hotelId)
+    .doc(providerId)
     .collection('customers')
     .doc(customerId)
     .collection('messages')
     .orderBy('createdAt', 'asc')
     .onSnapshot(snapData => {
       snapData.forEach(result => {
-        console.log('Nested data', result.data());
-      });
-    });
-
-  db.collection('messages')
-    .orderBy('createdAt', 'asc')
-    .onSnapshot(snapData => {
-      let newData: IMessage[] = [];
-
-      snapData.forEach(result => {
-        console.log('UID check: ', result.id);
-
         const msg = {
           id: result.id,
           createdAt: result.data().createdAt,
@@ -43,54 +31,110 @@
           receiver: result.data().receiver,
           type: result.data().type,
         } as IMessage;
-        newData = [...newData, msg];
+        msgData = [...msgData, msg];
+        console.log('Nested data', result.data());
       });
-      result = newData;
-      console.log('data result ', result);
-      // result = snapData.docs;
     });
 
-  function addMsg() {
-    //const date = Date.now();
-    //db.collection("messages").doc(doc.id)
-    const date = Date.now();
-    db.collection('messages').add({
-      createdAt: date,
-      msg: msg,
-      sender: 'sender ID',
-      receiver: 'receiver ID',
-      type: 'Order',
-    });
-    msg = '';
-  }
+  // db.collection('messages')
+  //   .orderBy('createdAt', 'asc')
+  //   .onSnapshot(snapData => {
+  //     let newData: IMessage[] = [];
+
+  //     snapData.forEach(result => {
+  //       console.log('UID check: ', result.id);
+
+  //       const msg = {
+  //         id: result.id,
+  //         createdAt: result.data().createdAt,
+  //         msg: result.data().msg,
+  //         sender: result.data().sender,
+  //         receiver: result.data().receiver,
+  //         type: result.data().type,
+  //       } as IMessage;
+  //       newData = [...newData, msg];
+  //     });
+  //     result = newData;
+  //     console.log('data result ', result);
+  //     // result = snapData.docs;
+  //   });
+
+  // function addMsg() {
+  //   //const date = Date.now();
+  //   //db.collection("messages").doc(doc.id)
+  //   const date = Date.now();
+  //   db.collection('messages').add({
+  //     createdAt: date,
+  //     msg: msg,
+  //     sender: 'sender ID',
+  //     receiver: 'receiver ID',
+  //     type: 'Order',
+  //   });
+  //   msg = '';
+  // }
+
+  const id  = "123abc"
 
   const addCustomerMsg = () => {
     const date = Date.now();
     db.collection('db_messages')
-      .doc(hotelId)
+      .doc(providerId)
       .collection('customers')
       .doc(customerId)
       .collection('messages')
       .add({
-        id: date,
+        id: id,
         createdAt: date,
-        msg: 'MEssage 2',
-        sender: hotelId,
+        msg: messageInput,
+        sender: providerId,
         receiver: customerId,
         type: 'Order',
       });
   };
-</script>
 
-<div class="overflow-scroll">
-  This is chat {user.name}
-  <input bind:value={msg} />
-  <button on:click={addMsg}>Submit</button>
-  <button on:click={addCustomerMsg}>Add CustomerMSg </button>
+  const onSendMsg = () => {
+      addCustomerMsg()
+  };
 
-  {#each result as r}
-    <div>{r.msg}</div>
+  const onUpdateMsg = (up_id) => {
+    const date = Date.now();
+    db.collection('db_messages')
+      .doc(providerId)
+      .collection('customers')
+      .doc(customerId)
+      .collection('messages')
+      .add({
+        id: up_id,
+        createdAt: date,
+        msg: messageInput,
+        sender: providerId,
+        receiver: customerId,
+        type: 'msg',
+      });
+  }
+
+</script>`
+
+<div>
+  <div class="overflow-scroll relative">
+  <!-- This is chat {user.name} -->
+  <button on:click={onUpdateMsg}>Update</button>
+  
+  {#each msgData as r}
+    <div>{r.msg} from {r.id} type = {r.type}</div>
+    <button on:click={() => onUpdateMsg(r.id)}>Update</button>
   {/each}
+  
+  <div class="h-10"></div>
+</div>
+<div class="flex justify-items-center items-center absolute bottom-14 bg-gray-primary w-screen">
+  <input class="bg-gray-primary inline appearance-none rounded w-full py-2 px-4 text-gray-700
+    leading-tight focus:outline-none text-sm" 
+    id="inline-full-name" placeholder="Enter message" type="text" bind:value={messageInput}>  
+  <div class="inline  pr-4" on:click={onSendMsg}>
+    <img class ="w-4 h-4" src = {'icons/paper-plane-solid.svg'} alt=""/>
+  </div>
+</div>
 </div>
 
 <style>
