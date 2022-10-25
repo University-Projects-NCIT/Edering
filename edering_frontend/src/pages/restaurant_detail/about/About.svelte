bf1f57cd5ac96c8a03528af1705e28a17b729357
karkigrishmin <grishminkarki7@gmail.com>
2022-10-25 23:49:06 +0545

    about page

--- edering_frontend/src/pages/restaurant_detail/about/About.svelte
+++ edering_frontend/src/pages/restaurant_detail/about/About.svelte
@@ -1,1 +1,38 @@
-<h1>About</h1>
+<script lang="ts">
+  import { LoadingUI } from 'components';
+  import Box from 'components/layouts/Box.svelte';
+  import StarRating from 'svelte-star-rating';
+  import type { IHotel } from 'types';
+  export let restaurant: IHotel;
+</script>
+
+{#if restaurant}
+  <Box className="pt-2 px-2 space-y-5">
+    <Box>
+      <h1 class="text-base font-medium">
+        {restaurant?.name}
+      </h1>
+    </Box>
+    <Box className="bg-gray-primary rounded-2xl p-4 ">
+      <Box>
+        <h3 class="text-sm font-medium mb-2">Address</h3>
+        <p class="text-xs">Mid-Baneshwor, Kathmandu</p>
+      </Box>
+    </Box>
+    <Box className="bg-gray-primary rounded-2xl p-4 ">
+      <Box>
+        <h3 class="text-sm font-medium mb-2">Timing</h3>
+        <p class="text-xs">Opening time: 10:00 AM</p>
+        <p class="text-xs">Closing time: 10:00 PM</p>
+      </Box>
+    </Box>
+    <Box className="bg-gray-primary rounded-2xl p-4 ">
+      <Box>
+        <h3 class="text-sm font-medium mb-2">Rating</h3>
+        <StarRating rating={3} config={{ size: 10, fullColor: '#FFA800' }} />
+      </Box>
+    </Box>
+  </Box>
+{:else}
+  <p>Data Not found.......</p>
+{/if}
