190ec0ef5210d63e86bde2ee36902335b7c0d5c3
karkigrishmin <grishminkarki7@gmail.com>
2022-10-29 23:15:20 +0545

    define comment, foodcategory, menu, provider and rating types

--- edering_frontend/src/types/foodCategory.types.ts
+++ edering_frontend/src/types/foodCategory.types.ts
@@ -1,5 +1,9 @@
+import type { IMenu } from './menu.types';
+
 export interface IFoodCategory {
   id?: string;
   c_name?: string;
-  image_uri_id?: string;
+  image_id?: string;
+  provider?: string;
+  menus?: IMenu[];
 }
--- edering_frontend/src/types/menu.types.ts
+++ edering_frontend/src/types/menu.types.ts
@@ -1,7 +1,5 @@
 export interface IMenu {
   id?: string;
-  category?: string;
   name?: string;
   price?: string;
-  image_uri_id?: string;
 }
