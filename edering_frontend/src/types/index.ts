af4a5a063501badebf881ff6246ed5413d38f760
karkigrishmin <grishminkarki7@gmail.com>
2022-10-30 13:11:03 +0545

    resolve conflict

--- edering_frontend/src/types/index.ts
+++ edering_frontend/src/types/index.ts
@@ -2,9 +2,14 @@ export * from './shared.types';
 export * from './text.types';
 export * from './request.types';
 export * from './user.types';
-export * from './hotels.types';
+export * from './provider.types';
 export * from './input.types';
 export * from './menu.types';
 export * from './message.types';
 export * from './orders.types';
+<<<<<<< HEAD
 export * from './ppp_models.type';
+=======
+export * from './comment.types';
+export * from './rating.types';
+>>>>>>> fb12793 (named export interface)
