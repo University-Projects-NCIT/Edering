d5efcf63bbb7d8556debc7d1feebf225190a7914
karkigrishmin <grishminkarki7@gmail.com>
2022-11-06 18:02:50 +0545

    minor fix of logo not showing issue

--- edering_frontend/src/pages/profile/CustomerProfile.svelte
+++ edering_frontend/src/pages/profile/CustomerProfile.svelte
@@ -1,23 +1,21 @@
-<script lang='ts'>
+<script lang="ts">
   import Box from 'components/layouts/Box.svelte';
   import { customer } from 'store';
   import type { IOrder } from 'types';
-  import dayjs from 'dayjs'
-  import BarLoader from 'svelte-loading-spinners/dist/ts/BarLoader.svelte';
+  import dayjs from 'dayjs';
 
-  export let onLogout = () => {}
+  export let onLogout = () => {};
 
-  $: orders  = $customer.customer_order  ?? []
+  $: orders = $customer.customer_order ?? [];
 
   const onClickHistory = () => {
-      //goto history 
-  }
-
+    //goto history
+  };
 </script>
 
-<Box className='flex flex-col'>
+<Box className="flex flex-col">
   <Box className="px-4">
-    <Box flow='horizontal' align='center'>
+    <Box flow="horizontal" align="center">
       <h1 class="text-2xl font-bold pb-4 mt-6">Customer Profile</h1>
       <Box
         onClick={onLogout}
@@ -26,30 +24,41 @@
         <img class="w-4" src="/icons/logout.svg" alt="" />
       </Box>
     </Box>
-    <Box flow='horizontal'>
-      <img 
+    <Box flow="horizontal">
+      <img
         class="inline h-20 w-20 rounded-lg"
-        src = { $customer.profile_image }
-        alt=""/>
-      <Box flow='vertical' className="pl-4">
+        src={$customer.profile_image}
+        alt=""
+      />
+      <Box flow="vertical" className="pl-4">
         <h1 class="text-sm">{$customer.name}</h1>
         <h1 class="text-sm">{$customer.email}</h1>
-        <h1 class="text-sm">Total orders: {$customer.customer_order?.length}</h1>
+        <h1 class="text-sm">
+          Total orders: {$customer.customer_order?.length}
+        </h1>
         <h1 class="text-sm">Total scans: {$customer.user_scan?.length}</h1>
-      </Box>  
+      </Box>
     </Box>
-    <Box flow='horizontal' justify='center' onClick={onClickHistory} className="p-4 rounded-xl mt-8 w-full bg-gray-primary">
+    <Box
+      flow="horizontal"
+      justify="center"
+      onClick={onClickHistory}
+      className="p-4 rounded-xl mt-8 w-full bg-gray-primary"
+    >
       <h1 class="text-md font-bold">Analyse History</h1>
     </Box>
   </Box>
-  <Box className='flex-1 bg-gray-primary rounded-tr-xl rounded-tl-xl p-4 mt-8'>
-    <Box className=''>
+  <Box className="flex-1 bg-gray-primary rounded-tr-xl rounded-tl-xl p-4 mt-8">
+    <Box className="">
       {#each orders as order}
         <Box>
-          <p class="text-sm inline">{order.order_to?.name}</p> <p class="inline text-sm">{dayjs(order.order_date_time ?? '').format('MMM D, YYYY (hh:mm A)')}</p>
-          <p class="text-sm block"> Cost: ${order.order_cost}</p>
+          <p class="text-sm inline">{order.order_to?.name}</p>
+          <p class="inline text-sm">
+            {dayjs(order.order_date_time ?? '').format('MMM D, YYYY (hh:mm A)')}
+          </p>
+          <p class="text-sm block">Cost: ${order.order_cost}</p>
         </Box>
-        <Box className="h-4"/>
+        <Box className="h-4" />
       {/each}
     </Box>
   </Box>
